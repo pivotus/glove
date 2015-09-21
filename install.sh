7f00f204fdbaf6eb2657e20c907a7a2e8f48aeb8
Emre Can Yılmaz <emrecan@ecylmz.com>
2015-09-21 16:54:14 +0300

    vim tagbar eklentisi için gerekli olan exuberant-ctags paketi kurulumda yüklensin.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Gerekli paketlerin yüklenmesi için parolanızı girin!"
 sudo apt-get update
 
 # install zsh, tmux
-sudo apt-get install -y zsh tmux vim vim-gtk build-essential cmake python-dev
+sudo apt-get install -y zsh tmux vim vim-gtk build-essential cmake python-dev exuberant-ctags
 
 # tmux 2.0
 wget http://ftp.de.debian.org/debian/pool/main/t/tmux/tmux_2.0-3_amd64.deb
