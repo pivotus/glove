12dfc3fe000f62dd86d270711e42eaa8f4f8456e
Emre Can Yılmaz <emrecan@ecylmz.com>
2015-10-07 14:27:36 +0300

    ln yerine cp komutu kullan

--- tools/powerline-font/install.sh
+++ tools/powerline-font/install.sh
@@ -5,8 +5,8 @@ sudo mkdir -p /usr/share/fonts/truetype/custom
 mkdir -p $HOME/.config/fontconfig/conf.d/
 
 echo "Installing font"
-sudo ln -s PowerlineSymbols.otf /usr/share/fonts/truetype/custom/
-ln -s 10-powerline-symbols.conf $HOME/.config/fontconfig/conf.d/
+sudo cp PowerlineSymbols.otf /usr/share/fonts/truetype/custom/
+cp 10-powerline-symbols.conf $HOME/.config/fontconfig/conf.d/
 
 echo "Updating font cache"
 sudo fc-cache -f -v
